dd4fffc03244c61ede08dcb59b155ae94995410d
Vaja Harish <vajaharish778@gmail.com>
2022-04-18 19:56:57 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function changeModeLight(event) {
+  document.getElementById("light-version").style.background = " #ffffff";
+  document.getElementById("name").style.color = " #000000";
+  document.getElementById("handle").style.color = " #8a97a3";
+  document.getElementById("count1").style.color = " #292f33";
+  document.getElementById("count2").style.color = " #292f33";
+  document.getElementById("count3").style.color = " #292f33";
+  document.getElementById("count4").style.color = " #292f33";
+  document.getElementById("description").style.color = " #000000";
+}
+
+function changeModeDark(event) {
+  document.getElementById("light-version").style.background = " #1f2937";
+  document.getElementById("name").style.color = " #ffffff";
+  document.getElementById("handle").style.color = " #8a97a3";
+  document.getElementById("count1").style.color = " #e5e5e5";
+  document.getElementById("count2").style.color = " #e5e5e5";
+  document.getElementById("count3").style.color = " #e5e5e5";
+  document.getElementById("count4").style.color = " #e5e5e5";
+  document.getElementById("description").style.color = " #e5e5e5";
+}
